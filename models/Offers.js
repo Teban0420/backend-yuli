30c413a935add9283c5e6b48015beabf4698d8bc
esteb <giraldoesteban044@gmail.com>
2024-04-21 20:07:47 -0500

    creacion tabla offers

---
+++ models/Offers.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db.js');
+
+const Offers = db.define('offer', {
+
+    title: {
+        type: Sequelize.DataTypes.STRING(40),
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: false
+    },
+    image: {
+        type: Sequelize.DataTypes.STRING(255)
+    },
+    dateStart: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+    },
+    dateEnd: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+    },
+});
+
+module.exports = Offers;
